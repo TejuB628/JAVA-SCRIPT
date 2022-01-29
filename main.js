a300060b646c48ed889be91661d4d571408b2a3f
TejuB628 <96355642+TejuB628@users.noreply.github.com>
2022-01-29 12:19:30 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("header");
+var firstname="Tejaswini";
+var lastname="Bethapudi";
+console.log(firstname);
+console.log(lastname);
+ var fullname= firstname.concat(lastname);
+ console.log(fullname);
+var a = 10;
+var b = 5;
+
